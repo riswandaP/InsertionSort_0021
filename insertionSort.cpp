a016291315a5db8cee9061d544381c092917d988
riswandaP <riswandaputra03@gmail.com>
2025-03-26 20:05:53 +0700

    Membuat Prosedur Input

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -3,3 +3,19 @@ using namespace std;
 
 int arr[20]; 
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+            else
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+    }
+}
