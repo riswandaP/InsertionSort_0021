e3c91e70fa9f5fd8f99382624d0ce785a4f2ce7f
riswandaP <riswandaputra03@gmail.com>
2025-03-26 20:26:09 +0700

    Membuat Prosedur Step 5

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -43,13 +43,14 @@ void insertionSort()
         temp = arr[i];
 
         j = i - 1;
-    }
-
+    
     while (j >= 0 && arr[j] > temp)
     {
         arr[j + 1] = arr[j];
         j--;
     }
 
-    arr
+    
+    arr[j + 1] = temp;
+    }
 }
