4b9174761ab3a08cde7dabd7bffb9c6f78761eca
riswandaP <riswandaputra03@gmail.com>
2025-03-26 20:31:41 +0700

    Membuat Fungsi Display

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -50,7 +50,24 @@ void insertionSort()
         j--;
     }
 
-    
+
     arr[j + 1] = temp;
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Total Passs = " << n - 1 << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+        cout << endl;
+}
+
