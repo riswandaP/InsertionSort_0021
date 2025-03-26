b7d4670018b43b2439a1476cc233eb3f1580ad6d
riswandaP <riswandaputra03@gmail.com>
2025-03-26 20:16:31 +0700

    Membuat Prosedur Step 1

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -17,5 +17,27 @@ void input()
         }
             else
             cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n";
+        }
+
+            cout << endl;
+            cout << "======================" << endl;
+            cout << "Masukkan Element Array" << endl;
+            cout << "======================" << endl;
+
+
+        for (int i = 0; i < n; i++)
+        {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
     }
+}
+
+void insertionSort()
+{
+
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++)
+        
 }
